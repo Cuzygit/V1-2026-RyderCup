8c59813a784dd9a037fd16d95082188cf1b23ff3
Cuzygit <cousins.jim@gmail.com>
2026-02-21 23:45:52 -0600

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Ryder Cup Social (V1)</h1>
+      <p>If you can see this page in Vercel, your deploy pipeline works.</p>
+      <p>Next: add Supabase env vars + roster/matches pages.</p>
+    </div>
+  );
+}
